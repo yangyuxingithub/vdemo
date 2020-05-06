f7a0210f995999be9915c48180f120be5bec6e2f
yangyuxin <2212546038@qq.com>
2020-05-06 17:11:58 +0800

    提交工程

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- <span>{{auth}}</span> -->
+    <p>子组件数值：{{cnum}}</p>
+    <button @click="dianji(auth)">AddParentBtn</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    auth: {
+      type: String,
+      default: "children"
+    },
+    cnum: {
+      type: Number,
+      default: 666
+    }
+  },
+  data() {
+    return {};
+  },
+  components: {
+
+  },
+  methods: {
+    dianji(e) {
+      console.log(e);
+      this.$emit("addParentNum", e);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- <span>{{message}}</span>
+    <todo-item :auth="write.title"></todo-item> -->
+    <p>父组件数值：{{num}}</p>
+    <button @click="addNum">按钮</button>
+    <todo-item @addParentNum="acceptNum" :cnum="num"></todo-item>
+    <p>父组件接受子组件发来的值：{{pnum}}</p>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  data() {
+    return {
+      message: "I am a parent",
+      write: {
+        title: "parent to chidren"
+      },
+      num: 0,
+      pnum: 7
+    };
+  },
+  components: {
+    TodoItem
+  },
+  methods: {
+    addNum() {
+      this.num = this.num + 1;
+    },
+    acceptNum(e){
+      console.log(this.pnum);
+      this.pnum = e;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
